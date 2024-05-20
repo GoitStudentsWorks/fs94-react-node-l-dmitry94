9c523e59e22ae05b871902d6e5759f72de9f04ca
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-05-20 10:33:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/digital-creators-front',
+    base: '/fs94-react-node-l-dmitry94',
 });
